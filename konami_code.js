0107937cf1f4ec7c5d23fd034921beda6ebc235b
Peter van Ooy <pevanooy@gmail.com>
2017-09-28 18:58:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
-  const myBody = document.getElementsByTagName('body')
 
-  myBody.addEventListener('keydown', onKeyDownHandler)
+  document.body.addEventListener('keydown', onKeyDownHandler)
 }
 
 function onKeyDownHandler(e) {
