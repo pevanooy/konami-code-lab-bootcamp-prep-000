a17b9ffc7942988ccacc4131ddb6fb490a0293ff
Peter van Ooy <pevanooy@gmail.com>
2017-09-28 18:55:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,13 +10,13 @@ function init() {
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
- 
+
   if (key === code[index]) {
     index++;
- 
+
     if (index === code.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
